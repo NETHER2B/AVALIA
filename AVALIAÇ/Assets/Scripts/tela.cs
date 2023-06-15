decd27ecdc6334755c52c800af396d9734902600
Leonardo Henrique <124589108+andreysimprota@users.noreply.github.com>
2023-06-15 17:23:02 -0300

    FINAL

---
+++ AVALIAÇ/Assets/Scripts/tela.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tela : MonoBehaviour
+{
+    public void buttonstart()
+    {
+        SceneManager.LoadScene("Samplescene");
+    }
+}
